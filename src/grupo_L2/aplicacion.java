30c5ffdc12fc2b2a85c1739f23a8b430d94d61a8
alvarohr113 <alvaro201102@gmail.com>
2024-09-28 17:05:39 +0200

    Primer commit, rama master

---
+++ src/grupo_L2/aplicacion.java
@@ -0,0 +1,12 @@
+package grupo_L2;
+
+public class aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Comienzo de practica");
+		System.out.println("Primer commit, rama master");
+
+	}
+
+}
